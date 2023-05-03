0cf5b5105692bfb338b9e09eae64d12efb5bf76b
EmirJussepp <jusseppemir@gmail.com>
2023-05-03 10:24:20 -0300

    Se agrega el proyecto

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+function FbotonSg() {
+  let uno = document.getElementById('botonSg');
+  if (uno.innerHTML == 'Dejar de seguir') 
+      uno.innerHTML = 'Seguir';
+  else uno.innerHTML = 'Dejar de seguir'; 
+}
+
+
+
+
+ 
+
+
+
+
+
+
